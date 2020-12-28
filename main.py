d7cad3c981cdb0dc6652fe33394f0825704ece96
maxthauer <maxthauer@biggerdog.local>
2020-12-28 13:19:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def home():
 		clientip = request.remote_addr
 		return clientip
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=False, port=8080)
+  app.run(host='0.0.0.0', debug=True, use_reloader=False, port=443)
