7811cbb4176113e3229709f38b650ffc4f25275f
maxthauer <maxthauer@biggerdog.local>
2020-12-28 12:33:45 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import *
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def home():
+	if request.headers.getlist("X-Forwarded-For"):
+		clientip = request.headers.getlist("X-Forwarded-For")[0]
+		return clientip
+	else:
+		clientip = request.remote_addr
+		return clientip
+if __name__ == '__main__':
+  app.run(debug=True, use_reloader=False, port=5000)
