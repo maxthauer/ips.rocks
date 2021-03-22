efa94271d5f82256aa74c99d247b0df7e2f60a24
maxthauer <maxthauer@gmail.com>
2021-03-22 17:23:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -119,4 +119,4 @@ def time():
 	return jsonify(epoch), 200
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=False, use_reloader=False, port=5000)
+  app.run(host='0.0.0.0', debug=False, use_reloader=False, port=443)
